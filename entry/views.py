01c99e493fbbfd5ef22e2193273dc49e01ff7ebc
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-10-26 01:17:59 +0300

    add pagination and filtration to entries

--- entry/views.py
+++ entry/views.py
@@ -4,15 +4,23 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 from django.utils import timezone
 from .models import Entry
 from .serializers import EntrySerializer
+
 # Create your views here.
 class EntryViewSet(viewsets.ModelViewSet):
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
     permission_classes = [IsAuthenticated]
-
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['status', 'category']
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at']
+    ordering = ['-created_at']
     def get_queryset(self):
         """
         Admins can see all entries.
